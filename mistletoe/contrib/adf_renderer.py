e97e3685adaff3cb375cae380d760c98df978f35
APS1990A <a.p.setiawan90@outlook.com>
2026-09-25 20:25:15 +0700

    Style changes, moved code into separate functions

--- mistletoe/contrib/adf_renderer.py
+++ mistletoe/contrib/adf_renderer.py
@@ -5,8 +5,7 @@ Abstract syntax tree renderer for mistletoe.
 import json
 from mistletoe import block_token
 from mistletoe.base_renderer import BaseRenderer
-from mistletoe.block_token import BlockToken, Paragraph, ThematicBreak
-from mistletoe.token import Token
+from mistletoe.block_token import BlockToken, ThematicBreak
 
 
 def determine_list_type(token, node):
@@ -16,7 +15,7 @@ def determine_list_type(token, node):
             node["attrs"] = {}
             node["attrs"]["order"] = int(leader)
             return "orderedList"
-   
+
     return "bulletList"
 
 
@@ -46,7 +45,7 @@ ADF_TYPE = {
     "ThematicBreak": "rule",
     "Quote": "blockquote",
     "AutoLink": "link",
-    "EscapeSequence": None
+    "EscapeSequence": None,
 }
 
 ADF_ATTRS = {
@@ -56,7 +55,7 @@ ADF_ATTRS = {
     "language": "language",
 }
 MARK_VALUES = ("em", "strong", "strike", "link", "code")
-TEXT_TYPE = ("text")
+TEXT_TYPE = "text"
 
 
 class AdfRenderer(BaseRenderer):
@@ -72,15 +71,52 @@ class AdfRenderer(BaseRenderer):
         return lambda token: ""
 
 
-def handle_table(token):   
+def handle_table(token):
     paragraph_token = block_token.Paragraph([])
     paragraph_token.children = token.children
     return [paragraph_token]
 
+
 def handle_marks(token, node, marks):
     marks = [] if marks is None else marks
     marks.append(node)
-    return get_adf(token.children[0], marks, blockquotes = node["type"] == "blockquote")
+    return get_adf(token.children[0], marks, blockquotes=node["type"] == "blockquote")
+
+
+def handle_children(token, node, marks):
+    if token.__class__.__name__ == "SetextHeading":
+        get_adf(ThematicBreak("---"))
+    if node["type"] == "tableCell" or node["type"] == "tableHeader":
+        token.children = handle_table(token)
+    node["content"] = [
+        get_adf(child, marks, blockquotes=node["type"] == "blockquote")
+        for child in token.children
+        if ADF_TYPE[child.__class__.__name__]
+    ]
+
+
+def process_node(token, node, marks):
+    if node["type"] == "doc":
+        node["version"] = 1
+
+    if "content" in vars(token) and node["type"] in TEXT_TYPE:
+        node["text"] = getattr(token, "content")
+    for attrname in token.repr_attributes:
+        if ADF_ATTRS.get(attrname, None) is not None:
+            node["attrs"] = {} if node.get("attrs", None) is None else node["attrs"]
+            node["attrs"][ADF_ATTRS[attrname]] = getattr(token, attrname)
+    if node["type"] in MARK_VALUES:
+        if node.get("attrs", None) is not None:
+            if len(node["attrs"]) == 0:
+                del node["attrs"]
+        return handle_marks(token, node, marks)
+    if "header" in vars(token):
+        header_row_token = getattr(token, "header")
+        for cell_token in header_row_token.children:
+            cell_token.is_header = True
+
+        token.children.insert(0, header_row_token)
+
 
 def get_adf(token, marks=None, blockquotes=False):
     """
@@ -104,44 +140,19 @@ def get_adf(token, marks=None, blockquotes=False):
     )
 
     if node["type"] is not None and not blockquotes:
-        if node["type"] == "doc":
-            node["version"] = 1
-
-        if "content" in vars(token) and node["type"] in TEXT_TYPE:
-            node["text"] = getattr(token, "content").replace("\n", "")
-        for attrname in token.repr_attributes:
-            if ADF_ATTRS.get(attrname, None) is not None:
-                node["attrs"] = {} if node.get("attrs", None) is None else node["attrs"]
-                node["attrs"][ADF_ATTRS[attrname]] = getattr(token, attrname)
-        if node["type"] in MARK_VALUES:  
-            if node.get("attrs", None) is not None:
-                if len(node["attrs"]) == 0:
-                    del node["attrs"]
-            return handle_marks(token, node, marks)
-        if "header" in vars(token):
-            header_row_token = getattr(token, "header")
-            for cell_token in header_row_token.children:
-                cell_token.is_header = True
-
-            token.children.insert(0, header_row_token)
+        process_node(token, node, marks)
 
     if token.children is not None:
-        if token.__class__.__name__ == "SetextHeading":
-            get_adf(ThematicBreak("---"))
-        if node["type"] == "tableCell" or node["type"] == "tableHeader":
-            token.children = handle_table(token)
-        node["content"] = ([
-            get_adf(child, marks, blockquotes = node["type"] == "blockquote")
-            for child in token.children
-            if ADF_TYPE[child.__class__.__name__]
-        ])
+        handle_children(token, node, marks)
 
     if node.get("attrs", None) is not None:
         if len(node["attrs"]) == 0:
             del node["attrs"]
-    if node.get("type", None) is None or (node.get("type", None) == "blockquote" and blockquotes):
-            node = node["content"][0]
+    if node.get("type", None) is None or (
+        node.get("type", None) == "blockquote" and blockquotes
+    ):
+        node = node["content"][0]
     if marks is not None and len(marks) > 0 and not isinstance(token, BlockToken):
         node["marks"] = marks
-    
+
     return node
